15a41e4abe7ddf3af45031561005ddb7d6b97901
LouieKim <hyeok0724.kim@gmail.com>
2019-01-15 18:55:16 +0900

    fourth

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/
-python3 manage.py runserver 172.31.41.197:80
+python3 manage.py runserver 34.220.120.193:80
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/
-python3 manage.py runserver 172.31.38.181:80
+python3 manage.py runserver 34.220.120.193:80
